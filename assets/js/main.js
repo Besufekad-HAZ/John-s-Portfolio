944071d818ed6e90e2be5a118da286795e6aae07
Besufekad-HAZ <besurye@gmail.com>
2024-10-13 23:56:21 +0300

    Fix some layout and styling issues

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,28 @@
 /*=============== EMAIL JS ===============*/
-const contactForm = document.getElementById("contact-form");
-contactMessage = document.getElementById("contact-message");
+const contactForm = document.getElementById('contact-form');
+const contactMessage = document.getElementById('contact-message');
 
 const sendEmail = (e) => {
   e.preventDefault();
 
   //serviceID -templateID - #form - publickey
-  emailjs.sendForm("", "", "", "");
+  emailjs.sendForm('', '', '', '');
 };
+
 /*=============== SHOW SCROLL UP ===============*/
 const scrollUp = () => {
-  const scrollUp = document.getElementById("scroll-up");
-  // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
-  this.scrollY >= 350
-    ? scrollUp.classList.add("show-scroll")
-    : scrollUp.classList.remove("show-scroll");
+  const scrollUp = document.getElementById('scroll-up');
+  if (scrollUp) {
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+    window.scrollY >= 350
+      ? scrollUp.classList.add('show-scroll')
+      : scrollUp.classList.remove('show-scroll');
+  }
 };
-window.addEventListener("scroll", scrollUp);
+window.addEventListener('scroll', scrollUp);
+
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
-const sections = document.querySelectorAll("section[id]");
+const sections = document.querySelectorAll('section[id]');
 
 const scrollActive = () => {
   const scrollDown = window.scrollY;
@@ -26,32 +30,35 @@ const scrollActive = () => {
   sections.forEach((current) => {
     const sectionHeight = current.offsetHeight,
       sectionTop = current.offsetTop - 58,
-      sectionId = current.getAttribute("id"),
+      sectionId = current.getAttribute('id'),
       sectionsClass = document.querySelector(
-        " .nav__list a[href*=" + sectionId + "]"
+        ' .nav__list a[href*=' + sectionId + ']'
       );
 
-    if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
-      sectionsClass.classList.add("active-link");
-    } else {
-      sectionsClass.classList.remove("active-link");
+    if (sectionsClass) {
+      if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+        sectionsClass.classList.add('active-link');
+      } else {
+        sectionsClass.classList.remove('active-link');
+      }
     }
   });
 };
-window.addEventListener("scroll", scrollActive);
+window.addEventListener('scroll', scrollActive);
+
 /*=============== SCROLL REVEAL ANIMATION ===============*/
 const sr = ScrollReveal({
-  origin: "top",
-  distance: "60px",
+  origin: 'top',
+  distance: '60px',
   duration: 2500,
   delay: 400,
   //reset: true, //Animation repeat
 });
 
-sr.reveal(".perfil, .contact__form");
-sr.reveal(".info", { origin: "left", delay: 800 });
-sr.reveal(".skills", { origin: "left", delay: 1000 });
-sr.reveal(".about", { origin: "right", delay: 1200 });
-sr.reveal(".projects__card, .services__card, .experience__card", {
+sr.reveal('.perfil, .contact__form');
+sr.reveal('.info', { origin: 'left', delay: 800 });
+sr.reveal('.skills', { origin: 'left', delay: 1000 });
+sr.reveal('.about', { origin: 'right', delay: 1200 });
+sr.reveal('.projects__card, .services__card, .experience__card', {
   interval: 100,
 });
